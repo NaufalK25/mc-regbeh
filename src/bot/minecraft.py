b6ae642d11828afc9eb0cd58f49ec809dc03efa0
NaufalK25 <naufalkateni2001@gmail.com>
2022-12-09 08:58:24 +0700

    change to union annotation to old way

--- src/bot/minecraft.py
+++ src/bot/minecraft.py
@@ -1,6 +1,6 @@
 import re
 from os import getenv
-from typing import Tuple
+from typing import Tuple, Union
 
 from discord import Color
 from dotenv import load_dotenv
@@ -17,7 +17,7 @@ def get_server_info(minecraft_server: str):
         return None
 
 
-def is_server_online(response: PingResponse | None):
+def is_server_online(response: Union[PingResponse, None]):
     return hasattr(response, 'version') and re.match(r'^\d+(\.\d+){1,2}$', response.version.name)
 
 
@@ -48,15 +48,15 @@ def get_players_name(players: PingResponse.Players):
     return '\n'.join(map(get_player_name, enumerate(players.sample))) if are_players_exists(players) else ''
 
 
-def are_there_online_players(response: PingResponse | None, server_status: str):
+def are_there_online_players(response: Union[PingResponse, None], server_status: str):
     return hasattr(response, 'players') and server_status == 'Online'
 
 
-def no_players_online(response: PingResponse | None, server_status: str):
+def no_players_online(response: Union[PingResponse, None], server_status: str):
     return not (are_there_online_players(response, server_status) and are_players_exists(response.players))
 
 
-def get_online_players(response: PingResponse | None, server_status: str) -> Tuple[bool, str, str]:
+def get_online_players(response: Union[PingResponse, None], server_status: str) -> Tuple[bool, str, str]:
     if no_players_online(response, server_status):
         return False, '', ''
 
